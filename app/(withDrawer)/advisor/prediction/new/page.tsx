2cef87f263286afe497e6c2d55fc574d8df98352
hoanglxm <>
2024-06-17 15:57:19 +0700

    fix build

--- app/(withDrawer)/advisor/prediction/new/page.tsx
+++ app/(withDrawer)/advisor/prediction/new/page.tsx
@@ -1,10 +1,6 @@
 
 
-import { clientSearchCollection } from "@/app/lib/firebase/firestore";
-import { closeWIPPreds, getMyWIPPreds } from "@/app/lib/server";
 import  { PredictCreationForm } from "@/app/ui/expert/create-prediction-form";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { revalidatePath } from "next/cache";
 
 export default async function Page() {
 
