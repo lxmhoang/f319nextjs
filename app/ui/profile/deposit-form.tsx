b99101d3fe143fc47aa8f72b56db69ad901a2dfe
hoanglxm <>
2024-07-14 16:46:34 +0700

    fff

--- app/ui/profile/deposit-form.tsx
+++ app/ui/profile/deposit-form.tsx
@@ -10,36 +10,30 @@ export default function DepositForm() {
   const { user, firebaseUser } = useAppContext()
 
   if (user == undefined) {
-      return (
-          <> Loading ... </>
-      )
+    return (
+      <> Loading ... </>
+    )
   }
 
 
   return (
     <div>
-    {/* {user.uid} */}
-    <div>
-        {/* <div className='mb-4'>{user.displayName}</div> */}
+      {user && (<p className="mb-4">Mã định danh duy nhất của bạn là {user.accessId.toUpperCase()}</p>)}
+      <p className='mb-4'>Để nạp tiền, chuyển khoản tới số tài khoản 00577718001, ngân hàng TP Bank với nội dung :
 
-        {user && (<p className="mb-4">Mã định danh duy nhất của bạn là {user.accessId.toUpperCase()}</p>)}
-        <p className='mb-4'>Để nạp tiền, chuyển khoản tới số tài khoản 00577718001, ngân hành TP Bank với nội dung : 
-        
-        </p>
+      </p>
 
-    <Blockquote className='mb-4'>
-      DEPOSIT {user.accessId.toUpperCase()}
-    </Blockquote>
+      <Blockquote className='mb-4'>
+        DEPOSIT {user.accessId.toUpperCase()}
+      </Blockquote>
 
-       Sau tối đa 10 phút bộ phận kế toán sẽ cập nhất số dư tài khoản cho quý nhà đầu tư, mọi thắc mắc xin liên hệ 0987333333
+      Sau tối đa 10 phút bộ phận kế toán sẽ cập nhất số dư tài khoản cho quý nhà đầu tư, mọi thắc mắc xin liên hệ 0987333333
 
-        <div className="">
+      <div className="">
 
-        </div>
+      </div>
 
     </div>
-
-</div>
   );
 
 }
