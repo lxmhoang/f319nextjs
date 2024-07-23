c3cb8c3794947eecd2eae887654f17e5f84fa76c
hoanglxm <>
2024-07-24 05:07:55 +0700

    fiaa

--- app/ui/rankView.tsx
+++ app/ui/rankView.tsx
@@ -13,7 +13,7 @@ export default function RankView({ rankData }: { rankData: BoardProps[] }) {
                             <TabItem title={<span className="text-xs">{item.title}</span>} key={index} >
                                 <div className="mx-auto justify-center mb-0">
                                     <div className="mt-0 mx-auto w-[350px]">
-                                        <Label value="* không bao gồm chuyên gia solo" className="text-xs " /></div>
+                                        <Label value="* chỉ bao gồm chuyên gia rank" className="text-xs " /></div>
                                     <LeaderBoard title={item.title} since={item.since} to={item.to} perPax={item.perPax} rewards={[]} data={item.data} />
                                 </div>
                             </TabItem>
